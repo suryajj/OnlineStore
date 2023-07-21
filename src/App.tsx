12d25cce026b512481df7a80f393cc852c1b6667
Surya Jammalamadaka <suryaj@Suryas-MacBook-Pro.local>
2023-07-21 16:15:41 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      
+      <NavBar/>
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,17 @@
-function NavBar(){
+export default function NavBar(){
     return (
         <div className="navBar">
-            <button className="navBarOptions" id="homePage">Home</button>
-            <button className="navBarOptions" id="categories">Categories</button>
-            <button className="navBarOptions" id="deals">Exlusive Deals</button>
-            <button className="navBarOptions" id="newItems">New Additions</button>
-            <button className="navBarOptions" id="account">Account</button>
-            <button className="navBarOptions" id="cart">Cart</button>
+            <button id="homePage" className="navBarOptions navBarOptionsL">Home</button>
+            <button className="navBarOptions navBarOptionsL" id="categories">Categories</button>
+            <button className="navBarOptions navBarOptionsL" id="deals">Exlusive Deals</button>
+            <button className="navBarOptions navBarOptionsL" id="newItems">New Additions</button>
+            <input type="text" className="navBarOptions navBarOptionsL" id="searchItem" placeholder="Search for items"/>
+            <button className="navBarOptions navBarOptionsR" id="account">Account</button>
+            <button className="navBarOptions navBarOptionsR" id="cart">Cart</button>
         </div>
     );
+
+
+    
 }
+
