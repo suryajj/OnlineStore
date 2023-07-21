c0650c4a9aeb4d6bd7104d61fbd344d7c464840d
Surya Jammalamadaka <suryaj@Suryas-MacBook-Pro.local>
2023-07-21 14:23:30 -0400

    First edit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+function NavBar(){
+    return (
+        <div className="navBar">
+            <button className="navBarOptions" id="homePage">Home</button>
+            <button className="navBarOptions" id="categories">Categories</button>
+            <button className="navBarOptions" id="deals">Exlusive Deals</button>
+            <button className="navBarOptions" id="newItems">New Additions</button>
+            <button className="navBarOptions" id="account">Account</button>
+            <button className="navBarOptions" id="cart">Cart</button>
+        </div>
+    );
+}
