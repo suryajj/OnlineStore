bbbd789684dde42cddedefd3e8c63678fad67924
Surya Jammalamadaka <suryaj@Suryas-MacBook-Pro.local>
2023-07-21 16:49:57 -0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import NavBar from "./components/NavBar";
-
+import MainDisplay from "./components/MainDisplay";
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <MainDisplay/>
     </div>
   );
 }
---
+++ src/components/MainDisplay.tsx
@@ -0,0 +1,15 @@
+export default function MainDisplay(){
+    
+    
+    return (
+        <div className="display">
+            
+        <image className="image1"/>
+
+        <div className="displayText">Where things are made just for you</div>
+
+        <image className="image2"/>
+
+        </div>
+    );
+}
