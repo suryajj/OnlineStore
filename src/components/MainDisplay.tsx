be45fad02489cd2f892c31d89aa5864c7ff16e60
Surya Jammalamadaka <suryaj@Suryas-MacBook-Pro.local>
2023-07-23 21:40:58 -0400

    ssss

--- src/components/MainDisplay.tsx
+++ src/components/MainDisplay.tsx
@@ -2,13 +2,13 @@ export default function MainDisplay(){
     
     
     return (
-        <div className="display inner">
+        <div className="display">
             
-            <image className="image1"/>
+            <img src="https://i.pinimg.com/originals/1d/8f/2f/1d8f2fff49dd29efb7cc5448a83d83a0.png" className="image1 w-96" />
 
-            <div className="displayText">Where things are made just for you</div>
+            <div className="displayText text-6xl text-teal-300">Where things are made just for you</div>
 
-            <image className="image2"/>
+            <img className="image2"/>
 
         </div>
     );
